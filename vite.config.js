b52287f2427a12d5b8e49d520951e75f35a94dfd
alimoeinkhah <127603441+alimoeinkhah@users.noreply.github.com>
2026-08-01 11:44:16 +0200

    Fix asset paths for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default {
 
     // Change to the name of your repository
     // According to https://vitejs.dev/guide/static-deploy.html 
-    base: '/polimi-gis/', 
+    base: './',
 
     publicDir: "public",
 
